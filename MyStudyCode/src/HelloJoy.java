4e5ca2b36d0484485e71f75dc2efda1dfe787028
somullick <somullick@paypal.com>
2022-07-01 20:52:17 +0530

    new files

--- MyStudyCode/src/HelloJoy.java
+++ MyStudyCode/src/HelloJoy.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class HelloJoy {
     public static void main(String[] args) {
+        String newa = "address.city.geo";
+        String[] objectFieldNameArray = newa.split("\\.");
+        Character s;
+        String string = "3[a]2[bc]";
+        s = string.charAt(0);
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
     }
 }
---
+++ MyStudyCode/src/google/arrays/Replace0ToFindMaxSequenceOf1.java
@@ -0,0 +1,7 @@
+import java.util.Stack;
+
+public class  Replace0ToFindMaxSequenceOf1{
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
